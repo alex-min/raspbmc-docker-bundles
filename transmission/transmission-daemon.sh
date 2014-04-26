3300ccfff756e452c2fafbcfe8028345b0f51dc9
MINETTE Alexandre <minette.alexandre@gmail.com>
2014-04-26 22:14:21 +0100

    changing the script

--- transmission/transmission-daemon.sh
+++ transmission/transmission-daemon.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 # launch transmission deamon
+exec chown debian-transmission:debian-transmission -R /var/downloads
+exec cp /etc/transmission-daemon/settings_override.json /etc/transmission-daemon/settings.json
 exec /usr/bin/transmission-daemon -f
