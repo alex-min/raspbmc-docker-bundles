91537b2cea9002c9d9679d800bdfe8ee1dead035
MINETTE Alexandre <minette.alexandre@gmail.com>
2014-04-26 22:18:02 +0100

    adding th

--- transmission/transmission-daemon.sh
+++ transmission/transmission-daemon.sh
@@ -2,4 +2,4 @@
 # launch transmission deamon
 exec chown debian-transmission:debian-transmission -R /var/downloads
 exec cp /etc/transmission-daemon/settings_override.json /etc/transmission-daemon/settings.json
-exec /usr/bin/transmission-daemon -f
+exec /usr/bin/transmission-daemon -f --config-dir /var/lib/transmission-daemon/info
